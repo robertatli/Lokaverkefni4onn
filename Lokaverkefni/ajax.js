aa6c8bcc9da263f1c5116f4e4ec67ad026e34020
robertatli <robbster10@gmail.com>
2017-04-07 12:47:47 +0000

    Er að vinni í því að adda filter tagging
    ennþá nokkur vandamál við að adda filter tagging.

---
+++ Lokaverkefni/ajax.js
@@ -0,0 +1,74 @@
+$.ajax({
+  'url': 'http://apis.is/concerts',
+  'type': 'GET',
+  'dataType': 'json',
+  'success': function(response) {
+    console.log(response);
+	for (var i = 0; i <= response.results.length; i++) {
+    	document.getElementById('images').innerHTML +=
+      "<div class='img'>"+
+        "<img src='"+
+        response.results[i].imageSource+
+        "' data-tags='"+
+        response.results[i].eventHallName+
+        "'><h2>"+
+        response.results[i].eventDateName+
+        "</h2>"+
+      "<div class='venue' id='venue'>"+
+      response.results[i].eventHallName+
+      "</div></div>"
+    }
+
+    (function() { // þarf að laga
+
+    var $imgs = $('#images div img');
+    var $buttons = $('#buttons');
+    var tagged = {};
+
+    $imgs.each(function(){
+      var img = this;
+      var tags = $(this).data('tags');    
+      
+
+      if (tags) {
+        tags.foreach(function(tagName) {
+        if (tagged[tagName] == null) {
+          tagged[tagName] = [];
+        }
+        tagged[tagName].push(img);
+        });
+      }
+    });
+
+    $('<button/>', {
+      text: 'Show all',
+      class: 'active',
+      click: function(){
+        $(this)
+        .addClass('active')
+        .siblings()
+        .removeClass('active');
+        $imgs.show();
+      }
+    }).appendTo($buttons);
+
+    $.each(tagged, function(tagName){
+      $('<button/>', {
+        text: tagName + ' (' + tagged[tagName].length + ')',
+        click: function() {
+          $(this)
+          .addClass('active')
+          .siblings()
+          .removeClass('active');
+          $imgs
+          .hide()
+          .filter(tagged[tagName])
+          .show();
+        }
+      }).appendTo($buttons);
+    })
+
+  }());
+  }
+
+});
