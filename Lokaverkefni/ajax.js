5f166c33c8ea1fa3a3c902c49c7c45f83ac6e026
robertatli <robbster10@gmail.com>
2017-04-30 18:37:53 +0000

    Reyndi að laga filter tags systemið
    Reyndi að laga filter tags systemið, setti inn auka testing síðu fyrir það, komst að því að það keyrir á sama tíma og $.ajax og getur því ekki fengið gögnin sem þarf til að gera til buttonana.

--- Lokaverkefni/ajax.js
+++ Lokaverkefni/ajax.js
@@ -3,10 +3,9 @@ $.ajax({
   'type': 'GET',
   'dataType': 'json',
   'success': function(response) {
-    console.log(response);
 	for (var i = 0; i <= response.results.length; i++) {
     	document.getElementById('images').innerHTML +=
-      "<div class='img'>"+
+      "<div id='img'>"+
         "<img src='"+
         response.results[i].imageSource+
         "' data-tags='"+
@@ -16,67 +15,62 @@ $.ajax({
         "</h2>"+
       "<div class='venue' id='venue'>"+
       response.results[i].eventHallName+
-      "</div><div class='dateVenue'>"+
+      "</div><div class='dateVenue'>| "+
       moment.utc(response.results[i].dateOfShow).format('LLL');
       moment.locale('is');
       +"</div></div>"
     }
 
+(function() { // runnar á sama tíma og $.ajax og virkar því ekki, meiri upplýsingar neðar
 
+  var $imgs = $('#images div img');
+  var $buttons = $('#buttons');
+  var tagged = {};
 
+  $imgs.each(function() {
+    var img = this;
+    var tags = $(this).data('tags');
 
-// img tags filter ------------------------ //
-
-    (function() { // þarf að laga
-
-    var $imgs = $('#images div img');
-    var $buttons = $('#buttons');
-    var tagged = {};
-
-    $imgs.each(function(){
-      var img = this;
-      var tags = $(this).data('tags');    
-      
-
-      if (tags) {
-        tags.foreach(function(tagName) {
+    if (tags) {
+      tags.split(',').forEach(function(tagName) {
         if (tagged[tagName] == null) {
           tagged[tagName] = [];
         }
         tagged[tagName].push(img);
-        });
-      }
-    });
+      });
+    }
+  });
 
-    $('<button/>', {
-      text: 'Show all',
-      class: 'active',
-      click: function(){
-        $(this)
+  $('<button/>', {
+    text: 'Show All',
+    class: 'active',
+    click: function() {
+      $(this)
         .addClass('active')
         .siblings()
         .removeClass('active');
-        $imgs.show();
-      }
-    }).appendTo($buttons);
+      $imgs.show();
+    }
+  }).appendTo($buttons);
 
-    $.each(tagged, function(tagName){
-      $('<button/>', {
-        text: tagName + ' (' + tagged[tagName].length + ')',
-        click: function() {
-          $(this)
+  $.each(tagged, function(tagName) {
+    $('<button/>', {
+      text: tagName + ' (' + tagged[tagName].length + ')',
+      click: function() {
+        $(this)
           .addClass('active')
-          .siblings()
-          .removeClass('active');
-          $imgs
+          .siblings()                 // Þetta á að velja öll element sem eru siblings af img
+          .removeClass('active');     // Þetta á að remova .active klasann frá siblingunum, þetta virkar ekki, removar bara img fallinu.
+        $imgs
           .hide()
           .filter(tagged[tagName])
           .show();
-        }
-      }).appendTo($buttons);
-    })
+      }
+    }).appendTo($buttons);
+  });
 
-  }());
+}());
+
   }
 
-});
+});// end $.ajax
